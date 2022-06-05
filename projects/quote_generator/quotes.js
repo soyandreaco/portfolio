514ac2f344f790a44ebcc937886275298ed0511c
soyandreaco <andsalazarco@gmail.com>
2022-06-05 12:00:44 -0500

    Update quote_generator version 1.0

---
+++ projects/quote_generator/quotes.js
@@ -0,0 +1,57 @@
+const localQuotes = [
+	
+	{
+		text: "La energía y la perseverancia lo conquistan todo.",
+		author: "Benjamin Franklin",
+	},
+
+	{
+		text: "Antes de reaccionar, piensa. Antes de gastar, gana. Antes de criticar, espera. Antes de abandonar, intenta.",
+		author: "Ernest Hemingway",
+	},
+
+	{
+		text: "El noventa por ciento del éxito se basa simplemente en insistir.",
+		author: "Woody Allen",
+	},
+
+	{
+		text: "Un optimista ve la oportunidad en toda calamidad; un pesimista ve una calamidad en toda oportunidad.",
+		author: "Winston Churchill",
+	},
+	{
+		text: "Sencillamente, no se puede vencer a quien nunca se rinde.",
+		author: "Babe Ruth",
+	},
+
+	{
+		text: "De todos los medios que conducen a la suerte, los más seguros son la perseverancia y el trabajo. ",
+		author: "Marie R. Reybaud",
+	},
+
+	{
+		text: "Algunas personas quieren que algo ocurra, otras sueñan con que pase, otras hacen que suceda.",
+		author: "Michael Jordan",
+	},
+
+	{
+		text: "Transforma tus heridas en sabiduría.",
+		author: "Oprah Winfrey",
+	},
+
+	{
+		text: "El hombre que mueve montañas comienza cargando pequeñas piedras",
+		author: "Confucio",
+	},
+
+	{
+		text: "El éxito es la suma de pequeños esfuerzos, que se repiten día tras día.",
+		author: "Robert Collier",
+	},
+
+	{
+		text: "Nunca es demasiado tarde para ser lo que podrías haber sido.",
+		author: "George Eliot",
+	},
+
+];
--- projects/quote_generator/script.js
+++ projects/quote_generator/script.js
@@ -1,1 +1,79 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+
+function newQuote() {
+  
+  // random number for index quotes
+  const quote = localQuotes[Math.floor(Math.random()*localQuotes.length)];
+  console.log(quote);
+  // Check if author field is blank and replace for desconocido
+  
+  if (!quote.author) {
+	authorText.textContent = "Desconocido";
+  } else {
+	authorText.textContent = quote.author;
+  }
+
+  // Check quote length to determine styling
+  if (quote.text.length > 80 ) {
+	quoteText.classList.add("long-quote");
+  } else {
+	quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.text;
+  
+}
+
+
+// Tweet quote
+function tweetQuote() {
+  const twitterUrl =  `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+
+// Event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
+newQuote();
+
+
+/* Code for use with API
+
+let apiQuotes = [];
+
+// Generate new newQuote
+function newQuote() {
+  // random number for index quotes
+  const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+  console.log(quote);
+
+}
+
+ Get quotes from API 
+
+async function getQuotes() {
+
+  const apiURL = "https://type.fit/api/quotes";
+  try {
+	const response = await fetch(apiURL);
+	apiQuotes = await response.json();
+	newQuote();
+  } catch (error) {
+	console.log("error");
+	// cath error here
+  }
+}
+
+// On load
+getQuotes();
+
+*/
 
