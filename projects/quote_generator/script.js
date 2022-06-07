4b22112bcf7452dd5c549b01b51ab3e0337e1970
soyandreaco <andsalazarco@gmail.com>
2022-06-06 21:23:31 -0500

    Update quote generator comments and name functions file js

--- projects/quote_generator/script.js
+++ projects/quote_generator/script.js
@@ -5,89 +5,79 @@ const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
 const loader = document.getElementById("loader");
 
-// Show loader
-function loading() {
-  loader.hidden = false;
-  quoteContainer.hidden = true;
+function showLoadingSpinner() {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
 }
 
-// Hide loader
-function completeLoader() {
-  loader.hidden = true;
-  quoteContainer.hidden = false;
+function removeLoadingSpinner() {
+	loader.hidden = true;
+	quoteContainer.hidden = false;
 }
 
-// Show new quote
 function newQuote() {
+	showLoadingSpinner();
 
-  loading();
+	// random number for index quotes
+	const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+	//console.log(quote);
+	// Check if author field is blank and replace for desconocido
 
-  // random number for index quotes
-  const quote = localQuotes[Math.floor(Math.random()*localQuotes.length)];
-  //console.log(quote);
-  // Check if author field is blank and replace for desconocido
-  
-  if (!quote.author) {
-	authorText.textContent = "Desconocido";
-  } else {
-	authorText.textContent = quote.author;
-  }
+	if (!quote.author) {
+		authorText.textContent = "Desconocido";
+	} else {
+		authorText.textContent = quote.author;
+	}
 
-  // Check quote length to determine styling
-  if (quote.text.length > 80 ) {
-	quoteText.classList.add("long-quote");
-  } else {
-	quoteText.classList.remove("long-quote");
-  }
-  
-  quoteText.textContent = quote.text;
-  //await delay(5);
-  completeLoader();
+	// Check quote length to determine styling
+	if (quote.text.length > 80) {
+		quoteText.classList.add("long-quote");
+	} else {
+		quoteText.classList.remove("long-quote");
+	}
+
+	quoteText.textContent = quote.text;
+	//await delay(5);
+	removeLoadingSpinner();
 }
 
-
-// Show new quote
 function getQuote() {
-  
-  loading();
-  // random number for index quotes
-  const quote = localQuotes[Math.floor(Math.random()*localQuotes.length)];
-  //console.log(quote);
-  // Check if author field is blank and replace for desconocido
-  
-  if (!quote.author) {
-	authorText.textContent = "Desconocido";
-  } else {
-	authorText.textContent = quote.author;
-  }
+	showLoadingSpinner();
+	// random number for index quotes
+	const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+	//console.log(quote);
+	// Check if author field is blank and replace for desconocido
 
-  // Check quote length to determine styling
-  if (quote.text.length > 80 ) {
-	quoteText.classList.add("long-quote");
-  } else {
-	quoteText.classList.remove("long-quote");
-  }
-  
-  quoteText.textContent = quote.text;
-  completeLoader();
+	if (!quote.author) {
+		authorText.textContent = "Desconocido";
+	} else {
+		authorText.textContent = quote.author;
+	}
+
+	// Check quote length to determine styling
+	if (quote.text.length > 80) {
+		quoteText.classList.add("long-quote");
+	} else {
+		quoteText.classList.remove("long-quote");
+	}
+
+	quoteText.textContent = quote.text;
+	removeLoadingSpinner();
 }
-
-
 
 // Tweet quote
 function tweetQuote() {
-  const twitterUrl =  `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-  window.open(twitterUrl, '_blank');
+	const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+	window.open(twitterUrl, "_blank");
 }
 
-
 // Event listeners
-newQuoteBtn.addEventListener('click', newQuote);
-twitterBtn.addEventListener('click', tweetQuote);
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
-loading();
+showLoadingSpinner();
+// pause 1 seg after call getQuote function
 setTimeout(getQuote, 1000);
-//getQuote();
 
 
 /* Code for use with API
@@ -121,4 +111,3 @@ async function getQuotes() {
 getQuotes();
 
 */
-
